bff1ac34010525c5d8d749d0b4cfaa264cbd0f2c
alazartsegaye <alazartsegaye0@gmail.com>
2025-02-11 14:21:19 -0800

    Utilizing the Django Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Book
 
+# Register the Book model with Django Admin
+admin.site.register(Book)
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
 
