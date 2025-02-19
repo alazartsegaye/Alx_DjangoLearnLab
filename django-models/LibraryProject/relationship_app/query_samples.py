37f797f0119238892b5bedb19b13efc1d8c3af7f
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-19 17:10:32 +0300

    Implementation of Sample Queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,18 +1,18 @@
 from relationship_app.models import Book,Library,Librarian
 
 # Query all books by a specific author
-def get_books_by_author(author_id):
-    books = Book.objects.filter(author_id=author_id)
+def get_books_by_author(name):
+    books = Book.objects.filter(name=author_name)
     return books
 
 # List all books in a library
-def def list_books_in_library(library_id):
+def def list_books_in_library(name):
     library = Library.objects.get(name=library_name)
     books = library.books.all()  # Assuming a ManyToMany relationship
     return books
 
 # Retrieve the librarian for a library
-def get_librarian_for_library(library_id):
-    library = Library.objects.get(name=library_name)
+def get_librarian_for_library(name):
+    library = Library.objects.get(name=librarian_name)
     librarian = library.librarian  # Accessing the related librarian
     return librarian
