5cec8d7dcbbc3f1eae29361d3fd479421e0b241a
alazartsegaye <alazartsegaye0@gmail.com>
2025-02-21 08:47:06 -0800

    Implementation of  Function-based View and Class-based View

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,11 +11,3 @@ class LibraryDetailView(DetailView):
     template_name = "library_detail.html"
     context_object_name = "library"
 
-from django.urls import path
-from .views import list_books, LibraryDetailView
-
-urlpatterns = [ 
-        path("books/", list_books, name="list_books"),
-        path("library/int:pk/", LibraryDetailView.as_view(), name="library_detail"),
-]
-
