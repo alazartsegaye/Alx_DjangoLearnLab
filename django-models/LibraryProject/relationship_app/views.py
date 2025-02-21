a3ffafbdfe502d30e72665ebd8e4268ff8390b3b
alazartsegaye <alazartsegaye0@gmail.com>
2025-02-21 23:34:08 +0300

    Implementation of Function-based View and Class-based View

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
 def list_books(request):
-    books = Book.objects.all()
-    return render(request, "list_books.html", {"books": books})
+    books = Book.objects.all()  # Fetch all books from the database
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
-    context_object_name = "library"
-
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'  # The context variable to use in the template
