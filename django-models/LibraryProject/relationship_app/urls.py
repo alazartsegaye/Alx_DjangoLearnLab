84219769ff2012a5a8fb3b8ba855e8078f8f0846
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-22 02:10:54 +0300

    URL patterns that route to the newly created views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView, register, admin_view, librarian_view, member_view
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import add_book, edit_book, delete_book
+
 
 urlpatterns = [ 
         path("books/", list_books, name="list_books"),
@@ -12,4 +14,7 @@ urlpatterns = [
         path('admin/', admin_view, name='admin_view'),
         path('librarian/', librarian_view, name='librarian_view'),
         path('member/', member_view, name='member_view'),
+        path('books/add/', add_book, name='add_book'),
+        path('books/edit/<int:book_id>/', edit_book, name='edit_book'),
+        path('books/delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
