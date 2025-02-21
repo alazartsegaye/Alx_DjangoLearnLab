fc2cf7004bb9313e3406c296107895a04bc481c6
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-22 00:13:27 +0300

    showcases complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields.

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name =  models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
@@ -9,14 +9,20 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    publication_year = models.IntegerField()
 
     def __str__(self):
-        return self.title
+        return f"{self.title} {self.author}"
 
 class Library(models.Model):
-    name = models.CharField(max_length=200)
+    name =  models.CharField(max_length=100)
     books = models.ManyToManyField(Book, related_name='libraries')
 
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.books}"
+
+class Librarian(models.Model):
+    name =  models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} {self.library}"
