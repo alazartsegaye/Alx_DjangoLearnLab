d30fc1809dcaafdaa2b16af8a3cc7f9ebba2627e
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-22 01:25:23 +0300

    Implementation of Function-based View and Class-based View

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,8 @@ from .models import Library
 from .models import Book
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .models import UserProfile
 
 # Function-based view to list all books
 def list_books(request):
@@ -47,3 +48,31 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
+
+# Helper function to check user role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+# Admin view
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+# Librarian view
+@login_required
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+# Member view
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
