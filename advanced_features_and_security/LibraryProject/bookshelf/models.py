c26f152ac127a6fd2264936c712781456958cbf3
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-25 14:36:42 +0300

    Updated models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .managers import CustomUserManager
 from django.contrib.auth.models import BaseUserManager
 
 class Book(models.Model):
