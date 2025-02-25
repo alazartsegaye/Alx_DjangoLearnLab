7cda99b0ec167d1311876de9a3109ed2f7e89a8a
alazartsegaye <alazartsegaye0@gmail.com>
2025-02-25 09:37:07 -0800

    A python file containing the definition of ExampleForm

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_year']
+
+    # Custom validation for the title field
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 3:
+            raise forms.ValidationError("Title must be at least 3 characters long.")
+        return title
+
+    # Custom validation for the author field
+    def clean_author(self):
+        author = self.cleaned_data.get('author')
+        if not author.replace(" ", "").isalpha():  # Ensure only letters
+            raise forms.ValidationError("Author name should contain only letters.")
+        return author
+
+    # Custom validation for publication year
+    def clean_publication_year(self):
+        year = self.cleaned_data.get('publication_year')
+        if year < 1500 or year > 2100:
+            raise forms.ValidationError("Publication year must be between 1500 and 2100.")
+        return year
+
