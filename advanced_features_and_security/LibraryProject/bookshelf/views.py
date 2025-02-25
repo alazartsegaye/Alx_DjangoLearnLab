3d3a16ea64cd60159a4b89c313095c9469a02089
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-02-25 17:36:34 +0300

    Updated views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,41 +1,40 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
-from .models import Library, Book
+from django.http import HttpResponseForbidden
+from .models import Book
 
-# View to list books in the library (requires 'can_view_books' permission)
-@permission_required('app_name.can_view_books', raise_exception=True)
-def library_books(request, library_id):
-    library = get_object_or_404(Library, id=library_id)
-    books = library.books.all()
-    return render(request, 'library_books.html', {'library': library, 'books': books})
+# View all books (Requires 'can_view' permission)
+@permission_required('app_name.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'book_list.html', {'books': books})
 
-# View to checkout a book (requires 'can_checkout_books' permission)
-@permission_required('app_name.can_checkout_books', raise_exception=True)
-def checkout_book(request, book_id):
-    book = get_object_or_404(Book, id=book_id)
-    # Simulate checking out the book
-    # (For simplicity, let's assume checking out simply marks the book as unavailable)
-    book.checked_out = True
-    book.save()
-    return redirect('library_books', library_id=book.library.id)
-
-# View to add a new book to the library (requires 'can_add_books' permission)
-@permission_required('app_name.can_add_books', raise_exception=True)
-def add_book_to_library(request, library_id):
-    library = get_object_or_404(Library, id=library_id)
+# Create a new book (Requires 'can_create' permission)
+@permission_required('app_name.can_create', raise_exception=True)
+def create_book(request):
     if request.method == 'POST':
         title = request.POST['title']
         author = request.POST['author']
-        description = request.POST['description']
-        new_book = Book.objects.create(title=title, author=author, description=description)
-        library.books.add(new_book)
-        return redirect('library_books', library_id=library.id)
-    return render(request, 'add_book.html', {'library': library})
+        publication_year = request.POST['publication_year']
+        Book.objects.create(title=title, author=author, publication_year=publication_year)
+        return redirect('book_list')
+    return render(request, 'create_book.html')
 
-# View to remove a book from the library (requires 'can_remove_books' permission)
-@permission_required('app_name.can_remove_books', raise_exception=True)
-def remove_book_from_library(request, library_id, book_id):
-    library = get_object_or_404(Library, id=library_id)
+# Edit a book (Requires 'can_edit' permission)
+@permission_required('app_name.can_edit', raise_exception=True)
+def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    library.books.remove(book)
-    return redirect('library_books', library_id=library.id)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.publication_year = request.POST['publication_year']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'edit_book.html', {'book': book})
+
+# Delete a book (Requires 'can_delete' permission)
+@permission_required('app_name.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
