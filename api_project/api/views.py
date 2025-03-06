55a77726d3d5b6c6feb309b710648fa5ca41f200
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-06 03:51:19 -0800

    Modify BookViewSet to include permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
-from rest_framework.generics import generics.ListAPIView
+from rest_framework.generics import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class BookList(ListAPIView):
     queryset = Book.objects.all()
@@ -9,3 +10,4 @@ class BookList(ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
