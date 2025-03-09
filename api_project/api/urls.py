8e25685d539248663c5c5127668c49596fd746c0
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-09 09:58:32 -0700

    Update the url pattern to register the BookViewSet with the router

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
-from .views import BookViewSet
+from .views import BookList, BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),
+    path('books_all/', BookViewSet.as_view({'get': 'list', 'post': 'create'}), name='book_all'),
 ]
