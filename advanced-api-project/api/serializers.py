41607c3ed96e4642d9b7a76cbda08f5e24f89137
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-12 20:53:47 +0300

    Update serializers to add ValidationError

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 from .models import Book, Author
 
 # BookSerializer: Serializes all fields of the Book model
@@ -6,6 +7,16 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book  # Specifies the model to be serialized
         fields = '__all__'  # Includes all fields of the Book model in the serialized output
+
+    def validate_publication_year(self, value):
+        """
+        Custom validation to ensure the publication_year is not in the future.
+        """
+        from django.utils import timezone
+        current_year = timezone.now().year  # Get the current year
+        if value > current_year:
+            raise ValidationError("Publication year cannot be in the future.")
+        return value
 
 # AuthorSerializer: Serializes the Author model and includes nested BookSerializer for related books
 class AuthorSerializer(serializers.ModelSerializer):
