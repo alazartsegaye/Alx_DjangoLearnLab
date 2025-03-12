41d4f73dbc3cae9d4668e537435f029a4a62800a
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-12 09:31:35 -0700

    Update model to add detail comment

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=255)
+    """
+    Model representing an author.
+    """
+    name = models.CharField(max_length=255)  # A string field to store the author's name, with a maximum length of 255 characters.
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    """
+    Model representing a book.
+    """
+    title = models.CharField(max_length=255)  # A string field to store the title of the book, with a maximum length of 255 characters.
+    publication_year = models.IntegerField()  # An integer field to store the year the book was published.
+    
+    # A foreign key linking to the Author model.
+    # This establishes a one-to-many relationship: one author can have multiple books.
+    # If the referenced author is deleted, all related books will also be deleted (due to on_delete=models.CASCADE).
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  
