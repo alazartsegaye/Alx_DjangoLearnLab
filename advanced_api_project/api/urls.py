8de6d087925ef794d78bee8f81028dffdc041beb
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-12 09:56:45 -0700

    Configure URLs for the api app

---
+++ advanced_api_project/api/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import (
+    BookListCreateView,
+    BookRetrieveUpdateDestroyView,
+    AuthorListCreateView,
+    AuthorRetrieveUpdateDestroyView,
+)
+
+urlpatterns = [
+    # Book URLs
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-retrieve-update-destroy'),
+
+    # Author URLs
+    path('authors/', AuthorListCreateView.as_view(), name='author-list-create'),
+    path('authors/<int:pk>/', AuthorRetrieveUpdateDestroyView.as_view(), name='author-retrieve-update-destroy'),
+]
