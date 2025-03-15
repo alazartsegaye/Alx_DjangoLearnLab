17ab4272393e749895d2432af03edfdf939341f9
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-15 16:41:10 +0300

    test: add unit tests for Book API endpoints

---
+++ advanced-api-project/api/test_views.py
@@ -0,0 +1,83 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Book, Author
+
+class BookTests(APITestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+        # Create a test author
+        self.author = Author.objects.create(name='Test Author')
+
+        # Create a test book
+        self.book = Book.objects.create(
+            title='Test Book',
+            author=self.author,
+            publication_year=2023
+        )
+
+        # Log in the test user
+        self.client.login(username='testuser', password='testpass')
+
+    def test_create_book(self):
+        url = reverse('book-create')
+        data = {
+            'title': 'New Book',
+            'author': self.author.id,
+            'publication_year': 2023
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Book.objects.count(), 2)
+
+    def test_retrieve_book(self):
+        url = reverse('book-detail', args=[self.book.id])  # Use the correct URL name for BookDetailView
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Test Book')
+
+    def test_update_book(self):
+        url = reverse('book-update', args=[self.book.id])  # Use the correct URL name for BookUpdateView
+        data = {'title': 'Updated Book'}
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.get(id=self.book.id).title, 'Updated Book')
+
+    def test_delete_book(self):
+        url = reverse('book-delete', args=[self.book.id])  # Use the correct URL name for BookDeleteView
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+
+    def test_unauthenticated_access(self):
+        # Log out the user
+        self.client.logout()
+
+        # Test unauthenticated access to BookCreateView
+        url = reverse('book-create')
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Test unauthenticated access to BookUpdateView
+        url = reverse('book-update', args=[self.book.id])
+        response = self.client.patch(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Test unauthenticated access to BookDeleteView
+        url = reverse('book-delete', args=[self.book.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_authenticated_access(self):
+        # Test authenticated access to BookListView
+        url = reverse('book-list')  # Use the correct URL name for BookListView
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Test authenticated access to BookDetailView
+        url = reverse('book-detail', args=[self.book.id])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
