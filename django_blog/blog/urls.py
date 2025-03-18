6206e37a7947a0bd5de69cf1a8ea4c9bef7829b9
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-18 23:03:04 +0300

    Configure URL patterns to handle comment-related actions

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
 from .views import register, profile, CustomLoginView, CustomLogoutView
 from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView,
+    PostListView, PostDetailView,
+    PostCreateView, PostUpdateView,
+    PostDeleteView, add_comment, 
+    CommentUpdateView, CommentDeleteView
 )
 
 urlpatterns = [
@@ -14,9 +13,13 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
 
-    path('posts/', PostListView.as_view(), name='post-list'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
