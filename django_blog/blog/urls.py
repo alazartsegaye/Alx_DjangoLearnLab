380119054153a4c121c9ae1139dec336f70e1f9a
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-18 23:42:15 +0300

    Update URL patterns to handle comment-related actions

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import register, profile, CustomLoginView, CustomLogoutView
-from .views import (
-    PostListView, PostDetailView,
-    PostCreateView, PostUpdateView,
-    PostDeleteView, add_comment, 
-    CommentUpdateView, CommentDeleteView
-)
+from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, 
+CommentUpdateView, CommentDeleteView)
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -19,8 +15,8 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
-    path('post/int:pk/comments/new/', add_comment, name='add-comment'),
-    path('comment/int:pk/update/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
 
