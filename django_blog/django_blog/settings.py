0bcd27e5403d82fb4dccc21827cc0684a37669dd
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-18 12:19:45 +0300

    configure project to properly serve static files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -121,6 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+TEMPLATES = [
+    {
+        'DIRS': [BASE_DIR / 'templates'],
+    },
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
