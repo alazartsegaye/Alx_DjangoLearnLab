d51fcf81baf416934f06573583c14a056db5e619
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-19 16:04:35 +0300

    Ensured the view fetches and orders posts correctly

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,52 +1,64 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login, logout
+from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from .forms import CustomUserCreationForm, ProfileForm, CommentForm
+from .forms import CustomUserCreationForm, CommentForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Comment, Tag
 from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.views import LogoutView
 
-# Registration View
+
+def home(request):
+    
+    latest_posts = Post.objects.all().order_by('-published_date')[:5]
+    context = {
+        'latest_posts': latest_posts,
+    }
+    return render(request, 'blog/home.html', context)
+
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # Log the user in after registration
-            return redirect('profile')  # Redirect to profile page
+            login(request, user) 
+            return redirect('profile') 
     else:
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
 
-# Login View (using Django's built-in view)
 from django.contrib.auth.views import LoginView
 
 class CustomLoginView(LoginView):
     template_name = 'blog/login.html'
     authentication_form = AuthenticationForm
 
-# Logout View (using Django's built-in view)
-from django.contrib.auth.views import LogoutView
-
 class CustomLogoutView(LogoutView):
     template_name = 'blog/logout.html'
 
-# Profile Management View
 @login_required
 def profile(request):
     if request.method == 'POST':
-        form = ProfileForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = ProfileForm(instance=request.user)
-    return render(request, 'blog/profile.html', {'form': form})
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
 
-# List all posts
+        user = request.user 
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+
+        messages.success(request, "Profile updated successfully!")
+        return redirect('profile')
+
+    return render(request, 'blog/profile.html', {'user': request.user})
+
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -63,10 +75,10 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     fields = ['title', 'content']
 
     def form_valid(self, form):
-        form.instance.author = self.request.user  # Set the author to the logged-in user
+        form.instance.author = self.request.user
         return super().form_valid(form)
 
-# Update a post (only for the author)
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
@@ -74,9 +86,9 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author  # Only the author can edit the post
+        return self.request.user == post.author 
 
-# Delete a post (only for the author)
+
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
@@ -84,7 +96,7 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 
     def test_func(self):
         post = self.get_object()
-        return self.request.user == post.author  # Only the author can delete the post
+        return self.request.user == post.author
     
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
@@ -93,16 +105,14 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     template_name = 'blog/add_comment.html'
 
     def form_valid(self, form):
-        # Get the post object from the URL
+
         post = get_object_or_404(Post, id=self.kwargs['post_id'])
-        # Set the post and author fields
         form.instance.post = post
         form.instance.author = self.request.user
-        # Save the comment
         return super().form_valid(form)
 
     def get_success_url(self):
-        # Redirect to the post detail page after a successful comment creation
+      
         return redirect('post-detail', pk=self.kwargs['post_id'])
 
 
