e6b1e56cc5ceed815660684e15cc24a0a2a77b56
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-19 14:44:04 +0300

    Modify URLs to include filtering post by tag

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import register, profile, CustomLoginView, CustomLogoutView
 from .views import (PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, CommentCreateView, 
-CommentUpdateView, CommentDeleteView)
+CommentUpdateView, CommentDeleteView, PostByTagListView)
 from . import views
 
 urlpatterns = [
@@ -10,14 +10,14 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('profile/', profile, name='profile'),
 
-    path('search/', views.search, name='search'),
-    path('tags/<str:tag_name>/', views.posts_by_tag, name='posts_by_tag'),
-
     path('post/', PostListView.as_view(), name='post-list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts_by_tag'),
 
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='add-comment'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='edit-comment'),
