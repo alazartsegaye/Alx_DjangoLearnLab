a80371b21b1d7b7184c24938ebd0c88fe91cb5b8
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-19 14:48:57 +0300

    Defined PostByTagListView to filter posts by tag in views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -6,7 +6,7 @@ from .forms import CustomUserCreationForm, ProfileForm, CommentForm
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Comment
+from .models import Post, Comment, Tag
 from django.db.models import Q
 
 # Registration View
@@ -125,6 +125,16 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
+
+class PostByTagListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        tag_slug = self.kwargs.get('tag_slug')
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        return Post.objects.filter(tags=tag)
     
 def search(request):
     query = request.GET.get('q')
