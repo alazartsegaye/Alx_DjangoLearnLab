6b9a1870c06ced4de8914f476c5a31adc557214f
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-26 14:50:38 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,6 +8,7 @@ from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 User = get_user_model()
 
@@ -40,29 +41,23 @@ class ProfileView(APIView):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
     
-@api_view(['POST'])
-def follow_user(request, user_id):
-    try:
-        user_to_follow = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+class UserDetailView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
 
-    if user_to_follow == request.user:
-        return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        user = self.request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
-    request.user.following.add(user_to_follow)
-    return Response({"detail": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
+    def post(self, request, *args, **kwargs):
+        user_id = request.data.get("user_id")
+        try:
+            user_to_follow = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
 
+        if user_to_follow == request.user:
+            return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['POST'])
-def unfollow_user(request, user_id):
-    try:
-        user_to_unfollow = User.objects.get(id=user_id)
-    except User.DoesNotExist:
-        return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
-
-    if user_to_unfollow == request.user:
-        return Response({"detail": "You cannot unfollow yourself."}, status=status.HTTP_400_BAD_REQUEST)
-
-    request.user.following.remove(user_to_unfollow)
-    return Response({"detail": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
+        request.user.following.add(user_to_follow)
+        return Response({"detail": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
