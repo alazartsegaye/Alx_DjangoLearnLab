c21968569975e53c8c571b829293be065135a52c
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-26 12:09:24 +0300

    Update serializers to include objects.create_user

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,8 +1,13 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+User = get_user_model().objects.create_user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -12,10 +17,14 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(  # Using create_user to ensure password hashing
+        user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password']
         )
-        Token.objects.create(user=user)  # Automatically create an auth token
+        Token.objects.create(user=user)  # Automatically create token
         return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
