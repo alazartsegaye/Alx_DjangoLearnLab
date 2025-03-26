5b48e1bf6d08e9afcd4c9866e865c6a70b9d3974
Alazar Tsegaye <alazartsegaye0@gmail.com>
2025-03-26 15:23:50 +0300

    Update urls for the feed path

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -4,11 +4,10 @@ from .views import PostViewSet, CommentViewSet, FeedViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet) 
-router.register(r'feed', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('<int:post_pk>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
     path('<int:post_pk>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
-
+    path('feed/', FeedViewSet.as_view, name='feed')
 ]
