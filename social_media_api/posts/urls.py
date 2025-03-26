2a0732782f36d6a435b2b1e6ab15bc88188f7135
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-26 14:27:44 +0300

    add a route to view the feed

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .views import PostViewSet, CommentViewSet, FeedViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet) 
+router.register(r'feed', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
