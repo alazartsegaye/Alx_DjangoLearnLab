2fc8b7bb256e9cfa6389f2264b11fc73e26a366c
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-26 18:36:58 +0300

    Define URL Patterns for Likes

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet, FeedViewSet
+from .views import PostViewSet, CommentViewSet, FeedViewSet, like_post, unlike_post
+
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet) 
-router.register(r'feed', FeedViewSet, basename='feed')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('<int:post_pk>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='comment-list'),
     path('<int:post_pk>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
-
+    path('feed/', FeedViewSet.as_view, name='feed'),
+    path('<int:post_pk>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='comment-detail'),
+    path('<int:pk>/like/', like_post, name='like-post'),
+    path('<int:pk>/unlike/', unlike_post, name='unlike-post'),
 ]
